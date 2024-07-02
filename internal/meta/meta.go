7c07541141c32fb4d813907e24645b54a7781e9a
Vik Norway <viknorway@gmail.com>
2024-07-02 22:25:50 +0300

    Change version

--- internal/meta/meta.go
+++ internal/meta/meta.go
@@ -2,7 +2,7 @@ package meta
 
 var (
 	// Version is the compile-time set version of Watchtower
-	Version = "v0.0.0-unknown"
+	Version = "v1.7.1.1vn"
 
 	// UserAgent is the http client identifier derived from Version
 	UserAgent string
